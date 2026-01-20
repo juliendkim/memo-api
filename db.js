5d8ef2a5abcc143f9724702451740235995fbb67
Dee Kim <deekim@cu.ac.kr>
2026-01-20 14:43:06 +0900

    added database features

---
+++ db.js
@@ -0,0 +1,22 @@
+import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const {Pool} = pg;
+const pool = new Pool({
+    host:process.env.DB_HOST,
+    port:process.env.DB_PORT,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME
+});
+pool.connect((err, client, release) => {
+    if(err) {
+        console.error(err.message);
+    } else {
+        console.log("DB Connected");
+        release();
+    }
+});
+
+export default pool;
--- memos.js
+++ memos.js
@@ -1,23 +1,67 @@
 import express from 'express';
+import pool from './db.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.send("read all memos"));
-router.get("/:id", (req, res) => {
-    const id = [req.params.id];
-    res.send(`read a memo with ${id}`);
+router.get('/', async (req, res) => {
+    var qry = "select * from memos";
+    try {
+        const result = await pool.query(qry);
+        res.send({success: true,data: result.rows});
+    } catch(err) {
+        res.send({success: false,message: err.message});
+    }
 });
 
-router.post('/', (req, res) => 
-    res.send("insert new memo")
-);
+router.get("/:id", async (req, res) => {
+    var qry = "select * from memos where id=$1";
+    try {
+        const result = await pool.query(qry, [req.params.id]);
+        if(result.rowCount === 0)
+            res.status(404).send({success: false, message:"not found"});
+        else
+            res.send({success: true,data: result.rows});
+    } catch(err) {
+        res.send({success: false,message: err.message});
+    }
+});
 
-router.put('/:id', (req, res) => 
-    res.send("update a memo with an id")
-);
+router.post('/', async (req, res) => {
+    var { id, title, content } = req.body;
+    var qry = "insert into memos(id, title, content) "
+               + "values($1, $2, $3) returning *";
+    try {
+        const result = await pool.query(qry, [id, title, content]);
+        res.status(201).send({success: true,data: result.rows[0]});
+    } catch(err) {
+        res.send({success: false,message: err.message});
+    }
+});
 
-router.delete('/:id', (req, res) => 
-    res.send("delete a memo with an id")
-);
+router.put('/:id', async (req, res) => {
+    var { title, content } = req.body;
+    var { id } = req.params;
+    var qry = "update memos set title=$1, content=$2 "
+               + "where id=$3 "
+               + "returning *";
+    try {
+        const result = await pool.query(qry, [title, content, id]);
+        res.send({success: true,data: result.rows[0]});
+    } catch(err) {
+        res.send({success: false,message: err.message});
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    var { id } = req.params;
+    var qry = "delete from memos where id=$1 "
+               + "returning *";
+    try {
+        const result = await pool.query(qry, [id]);
+        res.send({success: true,data: result.rows[0]});
+    } catch(err) {
+        res.send({success: false,message: err.message});
+    }
+});
 
 export default router;
